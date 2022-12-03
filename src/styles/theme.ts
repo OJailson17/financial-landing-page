8cf91d2160b7129459e7d8653e0689ba97e92c85
OJailson17 <JaylsonO17@gmail.com>
2022-12-03 00:07:42 -0300

    setup chakra ui

---
+++ src/styles/theme.ts
@@ -0,0 +1,11 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+	colors: {
+		blue: {
+			100: '#A5A6F6',
+			500: '#5D5FEF',
+			600: '#7879F1',
+		},
+	},
+});
